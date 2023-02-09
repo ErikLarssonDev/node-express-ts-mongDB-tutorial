3558ac984816dbcbe40b0c9fff06da4bb8c42c3a
Erik Larsson <erik.larsson@weknowit.se>
2023-02-09 16:43:11 +0100

    7. Basic nodejs | readline

--- BASIC_NODEJS/main.js
+++ BASIC_NODEJS/main.js
@@ -1,7 +1,20 @@
-console.log('text')
-global.console.log()
-global.process.stdout.write('What is node.js? \n')
-process.stdin.on('data', (data) => {
-    console.log(data.toString().trim())
+// console.log('text')
+// global.console.log()
+// global.process.stdout.write('What is node.js? \n')
+// process.stdin.on('data', (data) => {
+//     console.log(data.toString().trim())
+//     process.exit()
+// })
+
+const readline = require('readline')
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+
+rl.question("What's your name? ", (answer) => {
+    console.log(answer)
     process.exit()
 })
