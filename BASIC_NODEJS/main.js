54d34e2cab1e331abc788d7870c94489574e3f94
Erik Larsson <erik.larsson@weknowit.se>
2023-02-09 17:06:37 +0100

    10. Basic node.js | listing directory files

--- BASIC_NODEJS/main.js
+++ BASIC_NODEJS/main.js
@@ -14,7 +14,6 @@
 //     input: process.stdin,
 //     output: process.stdout
 // })
-
 // rl.question("What's your name? ", (answer) => {
 //     console.log(answer)
 //     process.exit()
@@ -23,12 +22,17 @@
 // 8. Basic node.js | path & util modules
 // const path = require('path')
 // const util = require('util')
-
 // console.log(path.basename(__filename))
 // console.log(__filename)
 // console.log(path.join(__dirname, "./path/file"))
 // console.log(util.log(path.basename(__filename)))
 
 // 9. Basic node.js | export and import
-const { fileExt } = require("./module.js");
-console.log(fileExt());
+// const { fileExt } = require("./module.js");
+// console.log(fileExt());
+
+// 10. Basic node.js | listing directory files
+/* FILE MANAGEMENT with fs module */
+const fs = require('fs')
+const files = fs.readdirSync("./")
+console.log(files)
